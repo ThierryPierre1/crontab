6d08ac29218b6d079bb694d4841c235c22b4abd8
Tpierre31 <thierry.pierre@stonybrook.edu>
2022-10-08 18:51:20 -0400

    Commit

--- cronjob.py
+++ cronjob.py
@@ -8,13 +8,21 @@ import json
 data = pd.read_json('https://healthdata.gov/resource/vbjv-rh5z.json')
 data
 
-## get current time
-now = time.time()
-timestart = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
-print('This program started running at: ', timestart)
-
 # get current working directory
 cwd = os.getcwd()
 
 # print cwd
 print(cwd)
+
+## get current time
+now = time.time()
+
+## Save time as string
+cTime = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
+print(cTime)
+
+data.to_csv('Data/Social_Determinants_and_Health_Equity_Resource_Guide.csv')
+
+# create a new file in the current working directory
+with open(cwd + '/testFile_' + cTime + '.txt', 'w') as f:
+    f.write(str(data))
